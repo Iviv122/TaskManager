6abdd35cede5545efd2713896cccb7ca20c38965
Ivan <ivan@MacBook-Air-Ivan.local>
2026-06-14 19:00:32 +0200

    Update time enum

---
+++ app/src/main/java/com/example/taskmanager/data/source/local/UpdateTime.kt
@@ -0,0 +1,15 @@
+package com.example.taskmanager.data.source.local
+
+
+public enum class UpdateTime (
+    val updateTime: Long,
+    val standartLabel: String,
+){
+    EveryMinute(60000,"Every minute"),
+    EveryFiveMinute(5*60000,"Every 5 minutes"),
+    EveryTenMinute(10*60000,"Every 10 minute"),
+    EveryFifteenMinute(15*60000,"Every 15 minute"),
+    EveryHalfHour(30*60000,"Every 30 minute"),
+    EveryHour(60*60000,"Every 60 minutes"),
+}
+
--- app/src/main/java/com/example/taskmanager/pages/SettingsScreen.kt
+++ app/src/main/java/com/example/taskmanager/pages/SettingsScreen.kt
@@ -1,9 +1,53 @@
 package com.example.taskmanager.pages
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import com.example.taskmanager.data.source.local.UpdateTime
+
 
 @Composable
-public fun SettingsScreen(){
-    Text("Settings")
+public fun SettingsScreen() {
+    var expanded by remember { mutableStateOf(false) }
+
+    Column(
+    ) {
+        Button(
+            onClick = { expanded = !expanded },
+        ) {
+            Text("Remind every X minutes")
+            Icon(
+                imageVector = Icons.Default.ArrowDropDown,
+                contentDescription = "dropDown"
+
+            )
+        }
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            UpdateTime.entries.forEach { item ->
+                DropdownMenuItem(
+                    text = { Text(item.standartLabel) },
+                    onClick = {
+                        expanded = false
+                    }
+                )
+            }
+        }
+    }
 }
